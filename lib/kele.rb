1562c925bfd970b30106cd12beeaf3d5b536d9dc
Peter McNeely <pete.mcneely@gmail.com>
2016-08-10 23:23:51 -0400

    Submit Checkpoint Fix to parameters

--- lib/kele.rb
+++ lib/kele.rb
@@ -62,12 +62,13 @@ class Kele
 		end
 	end
 
-	def create_submission checkpoint_id, assignment_branch, assignment_commit_link, comment
+	def create_submission checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id
 		body = {
-			'checkpoint_id': checkpoint_id,
+			'checkpoint_id': checkpoint_id.to_s,
 			'assignment_branch': assignment_branch,
 			'assignment_commit_link': assignment_commit_link,
-			'comment': comment
+			'comment': comment,
+			'enrollment_id': enrollment_id.to_s
 		}
 		response = self.class.post "/checkpoint_submissions", headers: {'authorization' => @auth_token}, body: body
 		begin
